e4d00b4132ba64339dd1c5992b66ac9b3ff489c3
manas6699 <manasbaroi6699@gmail.com>
2025-04-19 15:28:55 +0530

    Morya details added

--- src/app/morya/page.js
+++ src/app/morya/page.js
@@ -1,11 +1,27 @@
-import React from 'react'
 
+
+import React from 'react'
 import Navbar from '@/components/Navbar'
+
+import Hero from '@/componentsMorya/Hero'
+
+import Info from '@/componentsMorya/Info'
+
+import Location from '@/componentsMorya/Location'
+
+
+export const metadata= {
+  title: "4 & 3 BHK Apartments in Tolly gunge | Sugam Morya Phase 2",
+  description: "Sugam Morya in Tollygunge offers premium 3, 3.5  & 4  BHK homes with lush open areas, elegant design, and top-tier amenities on the prestigious BL Shah Road.",
+};
 
 const page = () => {
   return (
     <div>
         <Navbar source="morya"/>
+        <Hero/>
+        <Info/>
+        <Location/>
     </div>
   )
 }
