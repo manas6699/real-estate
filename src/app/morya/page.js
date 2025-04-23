aa138f331f8d9cc5ea3d88248e7ab42b7bd31e27
manas6699 <manasbaroi6699@gmail.com>
2025-04-23 18:52:02 +0530

    moriya sitemap and google search console verification added

--- src/app/morya/page.js
+++ src/app/morya/page.js
@@ -21,6 +21,9 @@ import Gallery from '@/componentsMorya/Gallery'
 export const metadata= {
   title: "4 & 3 BHK Apartments in Tolly gunge | Sugam Morya Phase 2",
   description: "Sugam Morya in Tollygunge offers premium 3, 3.5  & 4  BHK homes with lush open areas, elegant design, and top-tier amenities on the prestigious BL Shah Road.",
+  verification: {
+    google: 'Xy7W7IIbzhmM1foAbu7RHD7ZtvREjPomdnBe1dK467w', // Just the code, not full meta tag
+  },
 };
 
 const page = () => {
