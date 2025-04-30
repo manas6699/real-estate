a07267fe0282a95df3148f1df8ad3a91ce729ea9
manas6699 <manasbaroi6699@gmail.com>
2025-04-30 14:35:37 +0530

    meta data removed from page

--- src/app/morya/layout.tsx
+++ src/app/morya/layout.tsx
@@ -12,12 +12,12 @@ const poppins = Poppins({
 
 
 
-export const metadata= {
-  title: "4 & 3 BHK Apartments in Tolly gunge | Sugam Morya Phase 2",
-  description: "Sugam Morya in Tollygunge offers premium 3, 3.5  & 4  BHK homes with lush open areas, elegant design, and top-tier amenities on the prestigious BL Shah Road.",
-  verification: {
-    google: 'Xy7W7IIbzhmM1foAbu7RHD7ZtvREjPomdnBe1dK467w', // Just the code, not full meta tag
-  },
+export const metadata = {
+    title: "4 & 3 BHK Apartments in Tolly gunge | Sugam Morya Phase 2",
+    description: "Sugam Morya in Tollygunge offers premium 3, 3.5  & 4  BHK homes with lush open areas, elegant design, and top-tier amenities on the prestigious BL Shah Road.",
+    verification: {
+        google: 'Xy7W7IIbzhmM1foAbu7RHD7ZtvREjPomdnBe1dK467w', // Just the code, not full meta tag
+    },
 };;
 
 export default function RootLayout({
