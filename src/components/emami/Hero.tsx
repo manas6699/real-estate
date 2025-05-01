a6c976be6a90392b98ccf86c5f3d73801542af17
manas6699 <manasbaroi6699@gmail.com>
2025-05-01 12:51:02 +0530

    responsiveness fixed for hero

--- src/components/emami/Hero.tsx
+++ src/components/emami/Hero.tsx
@@ -6,16 +6,15 @@ const Hero = () => {
             className="relative w-full h-screen bg-cover bg-center"
             style={{ backgroundImage: 'url("/assets/emami/Hero.jpg")' }}
         >
-            <div className="flex flex-col md:flex-row justify-between items-center h-full p-4 md:p-10">
+            <div className="flex flex-col md:flex-row justify-center md:justify-between items-center h-full p-4 md:p-10">
                 {/* Heading */}
-                <h1 className="text-white text-4xl sm:text-5xl md:text-6xl lg:text-8xl text-center md:text-left font-extrabold mb-8 md:mb-0">
+                <h1 className="text-white text-6xl sm:text-5xl md:text-6xl lg:text-8xl text-center font-extrabold">
                     <span className="block">EMAMI</span>
                     AAMOD
                 </h1>
-
-                
             </div>
         </section>
+
 
     )
 }
