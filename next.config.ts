8ff61279c858751cc162cabd482a4006b6bf7d0d
manas6699 <manasbaroi6699@gmail.com>
2025-06-02 01:18:47 +0530

    Images from local domain and hosting domain added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
+    images: {
+        domains: [
+          'localhost',
+          'www.mmrrealty.co.in',
+          'cdn.example.com',
+          'api.example.io'
+        ],
+      },
 };
 
 export default nextConfig;
