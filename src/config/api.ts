6ee4be9327c762ed75a921be298b96bf8f7c3532
manas6699 <manasbaroi6699@gmail.com>
2025-06-02 01:21:30 +0530

    Post api added : localhost

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,6 @@
 export const API_BASE_URL = 'https://split-wise-clone-085p.onrender.com/api';
 export const LEADS_ENDPOINT = `${API_BASE_URL}/mmr/leads`;
 export const GET_ALL_LEADS = `${API_BASE_URL}/mmr/getallleads`;
+
+
+export const BACKEND_ADMIN_POST_API = `http://localhost:8000/api/mmr`;
