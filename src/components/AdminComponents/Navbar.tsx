c098508a5b294c9be758623f2682458d41b736a4
manas6699 <manasbaroi6699@gmail.com>
2025-06-02 20:50:52 +0530

    extra token check removed

--- src/components/AdminComponents/Navbar.tsx
+++ src/components/AdminComponents/Navbar.tsx
@@ -20,10 +20,6 @@ const Navbar = () => {
                 .find(row => row.startsWith('token='))
                 ?.split('=')[1];
 
-            if (!token) {
-                throw new Error('Token not found in cookies');
-            }
-
             await axios.post(
                 `${API_BASE_URL}/auth/logout`,
                 {},
@@ -41,33 +37,33 @@ const Navbar = () => {
             console.error('Logout failed:', err);
         }
     };
-    
-  return (
-    <>
-          <nav className="w-full bg-white shadow-md py-3">
-              <div className="container mx-auto px-4 flex items-center justify-between">
-                  {/* Logo */}
-                  <Link href="/" className="flex items-center">
-                      <Image src={Logo} alt="Logo" width={50} height={50} />
-                  </Link>
 
-                  {/* ADMIN PANEL text: hidden on small screens */}
-                  <div className="hidden lg:flex items-center gap-2 text-gray-800 font-semibold">
-                      ADMIN PANEL
-                  </div>
+    return (
+        <>
+            <nav className="w-full bg-white shadow-md py-3">
+                <div className="container mx-auto px-4 flex items-center justify-between">
+                    {/* Logo */}
+                    <Link href="/" className="flex items-center">
+                        <Image src={Logo} alt="Logo" width={50} height={50} />
+                    </Link>
 
-                  {/* Logout button */}
-                  <button
-                      onClick={handleLogout}
-                      className="flex items-center gap-1 text-red-400 hover:text-red-600 transition cursor-pointer"
-                  >
-                      <LogOut className="w-5 h-5" />
-                      <span className="hidden sm:inline">Logout</span>
-                  </button>
-              </div>
-          </nav>
-    </>
-  )
+                    {/* ADMIN PANEL text: hidden on small screens */}
+                    <div className="hidden lg:flex items-center gap-2 text-gray-800 font-semibold">
+                        ADMIN PANEL
+                    </div>
+
+                    {/* Logout button */}
+                    <button
+                        onClick={handleLogout}
+                        className="flex items-center gap-1 text-red-400 hover:text-red-600 transition cursor-pointer"
+                    >
+                        <LogOut className="w-5 h-5" />
+                        <span className="hidden sm:inline">Logout</span>
+                    </button>
+                </div>
+            </nav>
+        </>
+    )
 }
 
 export default Navbar
