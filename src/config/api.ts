f78adeb484dcacf55797309a79edf793be5eb0b6
manas6699 <manasbaroi6699@gmail.com>
2025-06-03 17:36:39 +0530

    production api changed

--- src/config/api.ts
+++ src/config/api.ts
@@ -3,6 +3,9 @@ export const LEADS_ENDPOINT = `${API_BASE_URL}/mmr/leads`;
 export const GET_ALL_LEADS = `${API_BASE_URL}/mmr/getallleads`;
 
 
-export const BACKEND_ADMIN_POST_API = `http://localhost:8000/api/mmr`;
+export const BACKEND_ADMIN_POST_API = `https://split-wise-clone-085p.onrender.com/api/mmr/create`;
 
-export const UPLOAD_IMAGES_BASE_URL = `http://localhost:8000/uploads`;
+export const UPLOAD_IMAGES_BASE_URL = `https://split-wise-clone-085p.onrender.com/api/mmr/uploads`;
+// export const BACKEND_ADMIN_POST_API = `http://localhost:8000/api/mmr`;
+
+// export const UPLOAD_IMAGES_BASE_URL = `http://localhost:8000/uploads`;
