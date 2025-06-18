e1b9c493c54fd599691fb1bd98400b87b8ba0763
manas6699 <manasbaroi6699@gmail.com>
2025-06-18 16:31:19 +0530

    Navbar transparent logo added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ import Image from 'next/image';
 import { X, Phone } from 'lucide-react';
 import 'react-toastify/dist/ReactToastify.css';
 
-import Logo from '../../public/assets/logo.png'
+import Logo from '../../public/assets/logo-transparent.png'
 import { LEADS_ENDPOINT } from '@/config/api';
 import { toast, ToastContainer } from 'react-toastify';
 
@@ -62,7 +62,7 @@ export default function Navbar(source: sourceType) {
     };
     
     return (
-        <nav className="w-full bg-white shadow-md py-4">
+        <nav className="w-full shadow-md py-4">
             <div className="container mx-auto px-4 flex flex-row lg:flex-row items-center justify-between">
 
                 {/* Left: Logo */}
