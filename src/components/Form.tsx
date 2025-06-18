2512aa70a484e9701b3370bea6ab28d18f2ca8e9
manas6699 <manasbaroi6699@gmail.com>
2025-06-18 16:30:37 +0530

    MMR Logo added to side form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -7,6 +7,8 @@ import Loader from '@/components/loader';
 import { LEADS_ENDPOINT } from '@/config/api';
 import React, { useState, useEffect } from 'react';
 import { ShieldCheck, Ban, Clock } from 'lucide-react';
+import Image from 'next/image';
+import Logo from '../../public/assets/logo-transparent.png';
 
 
 type BrochureFormData = {
@@ -59,16 +61,21 @@ export default function Form({ source }: FormProps) {
         }
     };
 
-
     return (
         <>
-            <div className="flex justify-center items-start mt-6 sm:mt-10 px-2 sm:px-4">
+        <div className="items-center align-middle justify-center flex">
+            <Image src={Logo} width={50} height={500} alt='logo' />
 
+        </div>
+            <div className="flex justify-center items-start mt-6 sm:mt-10 px-2 sm:px-4">
                 <form
                     onSubmit={handleSubmit}
-                    className="bg-gradient-to-br from-blue-500 via-purple-500 to-indigo-600 shadow-lg rounded-xl p-4 sm:p-6 w-full max-w-sm flex flex-col gap-4"
+                    className=" p-4 sm:p-6 w-full max-w-sm flex flex-col gap-4"
                 >
-                    <h2 className="text-2xl font-semibold text-center mb-2 text-white">Contact Us</h2>
+                    <h2 className="text-2xl font-semibold text-center mb-2">Yes , I am Interested.</h2>
+                    <p className='text-xs text-center text-gray-700'>
+                        Please provide whatspp enabled phone number for more details.
+                    </p>
 
                     <input
                         type="text"
@@ -77,7 +84,7 @@ export default function Form({ source }: FormProps) {
                         onChange={handleChange}
                         placeholder="Your Name"
                         required
-                        className="bg-white px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-pink-400 placeholder:text-sm"
+                        className="bg-transparent border-0 border-b border-gray-400 focus:border-pink-500 focus:outline-none focus:ring-0 px-1 py-2 placeholder:text-sm"
                     />
                     <input
                         type="email"
@@ -86,7 +93,7 @@ export default function Form({ source }: FormProps) {
                         onChange={handleChange}
                         placeholder="Email Address"
                         required
-                        className="bg-white px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-pink-400 placeholder:text-sm"
+                        className="bg-transparent border-0 border-b border-gray-400 focus:border-pink-500 focus:outline-none focus:ring-0 px-1 py-2 placeholder:text-sm"
                     />
                     <input
                         type="tel"
@@ -96,16 +103,16 @@ export default function Form({ source }: FormProps) {
                         placeholder="Your phone number (10 Digit)"
                         pattern="[0-9]{10}"
                         required
-                        className="bg-white px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-pink-400 placeholder:text-sm"
+                        className="bg-transparent border-0 border-b border-gray-400 focus:border-pink-500 focus:outline-none focus:ring-0 px-1 py-2 placeholder:text-sm"
                     />
 
                     <button
                         type="submit"
-                        className="bg-[#de3163] cursor-pointer hover:bg-[#c42553] text-white py-2 rounded-md transition-all"
+                        className="bg-[#de3163] text-white cursor-pointer hover:bg-[#c42553] py-2 rounded-md transition-all"
                     >
                         {loading ? (
                             <div className="flex justify-center items-center">
-                                <Loader color='white' />
+                                <Loader color="white" />
                             </div>
                         ) : (
                             <>Submit</>
@@ -113,47 +120,42 @@ export default function Form({ source }: FormProps) {
                     </button>
                 </form>
             </div>
-            <div className="mt-14 text-center font-extrabold space-y-2">
-                <p className="text-xl font-extrabold sm:text-base text-white">
+            <div className="mt-6 text-center font-extrabold space-y-2">
+                <p className="text-xl font-extrabold sm:text-base  ">
                     ✨ Our expert team will reach out to you within 24 hours.
                 </p>
-                
-
-
                 <div className="flex justify-center gap-4 flex-wrap mt-4 text-xs sm:text-sm">
                     {/* Privacy */}
-                    <span className="flex items-center gap-2 bg-white/20 backdrop-blur-md border border-white/30 text-white px-5 py-1 rounded-full shadow-md">
+                    <span className="flex items-center gap-2 bg-white/20 backdrop-blur-md border border-white/30   px-5 py-1 rounded-full shadow-md">
                         <div className="relative flex items-center justify-center w-4 h-4">
                             <span className="absolute inline-flex h-3 w-3 rounded-full bg-green-400 opacity-75 animate-ping"></span>
                             <span className="relative inline-flex h-2 w-2 rounded-full bg-green-500"></span>
                         </div>
-                        <ShieldCheck className="w-4 h-4 text-white" />
+                        <ShieldCheck className="w-4 h-4  " />
                         100% Privacy Guaranteed
                     </span>
 
                     {/* No Spam */}
-                    <span className="flex items-center gap-2 bg-white/20 backdrop-blur-md border border-white/30 text-white px-5 py-1 rounded-full shadow-md">
+                    <span className="flex items-center gap-2 bg-white/20 backdrop-blur-md border border-white/30   px-5 py-1 rounded-full shadow-md">
                         <div className="relative flex items-center justify-center w-4 h-4">
                             <span className="absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75 animate-ping"></span>
                             <span className="relative inline-flex h-2 w-2 rounded-full bg-pink-500"></span>
                         </div>
-                        <Ban className="w-4 h-4 text-white" />
+                        <Ban className="w-4 h-4  " />
                         No Spam Policy
                     </span>
 
                     {/* Instant Delivery */}
-                    <span className="flex items-center gap-2 bg-white/20 backdrop-blur-md border border-white/30 text-white px-5 py-1 rounded-full shadow-md">
+                    <span className="flex items-center gap-2 bg-white/20 backdrop-blur-md border border-white/30   px-5 py-1 rounded-full shadow-md">
                         <div className="relative flex items-center justify-center w-4 h-4">
                             <span className="absolute inline-flex h-3 w-3 rounded-full bg-yellow-400 opacity-75 animate-ping"></span>
                             <span className="relative inline-flex h-2 w-2 rounded-full bg-yellow-500"></span>
                         </div>
-                        <Clock className="w-4 h-4 text-white" />
+                        <Clock className="w-4 h-4  " />
                         Instant Brochure Delivery
                     </span>
                 </div>
-
             </div>
         </>
-
     );
 }
