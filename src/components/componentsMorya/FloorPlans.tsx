f6e874b5009e2e67375aec12edab39d662b44e24
manas6699 <manasbaroi6699@gmail.com>
2025-06-18 16:34:26 +0530

    Housekeeping

--- src/components/componentsMorya/FloorPlans.tsx
+++ src/components/componentsMorya/FloorPlans.tsx
@@ -19,7 +19,7 @@ const images = [
 export default function GalleryMorya() {
     return (
         <div className="p-6">
-            <h1 className="text-4xl text-center font-bold mb-10">Floor Plans</h1>
+            <h1 className="text-4xl  font-bold mb-10">Floor Plans</h1>
             <ImageGallery images={images} />
         </div>
     );
