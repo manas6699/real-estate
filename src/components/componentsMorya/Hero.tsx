1437b0f537df96cf1563839cae2cf40f91ee1761
manas6699 <manasbaroi6699@gmail.com>
2025-06-19 13:29:27 +0530

    Video source changed

--- src/components/componentsMorya/Hero.tsx
+++ src/components/componentsMorya/Hero.tsx
@@ -26,7 +26,7 @@ const Hero = () => {
           muted={isMuted}
           className="absolute inset-0 w-full h-full object-cover"
         >
-          <source src="https://fynbnk.com/morya.mp4" type="video/mp4" />
+          <source src="https://fynbnk.com/morya-final.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
