da5635df4fd67ee2b360a8c35069409eed7d23de
manas6699 <manasbaroi6699@gmail.com>
2025-07-02 15:46:01 +0530

    s3 bucket domains added

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
         domains: [
           'localhost',
           'www.mmrrealty.co.in',
+          'split-wise-clone-085p.onrender.com',
+          'mmr-bucket-33001.s3.ap-south-1.amazonaws.com',
+          'mmr-bucket-33001.s3.amazonaws.com',
         ],
       },
 };
