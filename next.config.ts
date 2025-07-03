abca2f85bcb9a0e06446867768889c1858fa7309
manas6699 <manasbaroi6699@gmail.com>
2025-07-03 17:10:25 +0530

    refactor: update image configuration to use remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        domains: [
-          'localhost',
-          'www.mmrrealty.co.in',
-          'split-wise-clone-085p.onrender.com',
-          'mmr-bucket-33001.s3.ap-south-1.amazonaws.com',
-          'mmr-bucket-33001.s3.amazonaws.com',
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000', // or whatever port your local server runs on
       },
+      {
+        protocol: 'https',
+        hostname: 'www.mmrrealty.co.in',
+      },
+      {
+        protocol: 'https',
+        hostname: 'split-wise-clone-085p.onrender.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'mmr-bucket-33001.s3.ap-south-1.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'mmr-bucket-33001.s3.amazonaws.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
