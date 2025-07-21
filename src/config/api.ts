78587fe1ce5c88d32c1e94889b106598ba2c742a
manas6699 <manasbaroi6699@gmail.com>
2025-07-21 14:00:30 +0530

    api base url changed to production

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
-// export const API_BASE_URL = 'https://split-wise-clone-085p.onrender.com/api';
-export const API_BASE_URL = 'http://localhost:8000/api';
+export const API_BASE_URL = 'https://split-wise-clone-085p.onrender.com/api';
+// export const API_BASE_URL = 'http://localhost:8000/api';
 
 
 export const LEADS_ENDPOINT = `${API_BASE_URL}/mmr/leads`;
