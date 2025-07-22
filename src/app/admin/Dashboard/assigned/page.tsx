9abcc3d21dda09fc2795175127efba5cb2afc8f7
manas6699 <manasbaroi6699@gmail.com>
2025-07-22 12:19:39 +0530

    refactor: update AssignPage component by renaming telecaller fields to assignee and removing unused header

--- src/app/admin/Dashboard/assigned/page.tsx
+++ src/app/admin/Dashboard/assigned/page.tsx
@@ -9,11 +9,12 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { SHOW_ALL_ASSIGNS_API } from '@/config/api';
 
+
 type Assign = {
     _id: string;
     lead_id: string;
-    telecaller_id: string;
-    telecaller_name: string;
+    assignee_id: string;
+    assignee_name: string;
     status: string;
     remarks: string;
     history: string[];
@@ -52,19 +53,10 @@ const AssignPage = () => {
             <main className="flex flex-col md:flex-row w-full min-h-screen bg-gray-100">
                 <Sidebar />
                 <div className="flex-1 p-4">
-                    <div className="flex">
-
-                        <h1 className="text-2xl font-bold mb-4">Assigned Leads</h1>
-                        <div className="bg-black rounded-full h-8 ml-2.5 w-8">
-                            <text className="text-white text-xs font-extrabold flex items-center justify-center h-full">
-                                <span className="text-center">
-                                    10
-                                </span>
-                            </text>
-                        </div>
-                    </div>
+                    
                     <AssignedTable  data={assigns} />
                 </div>
+            
             </main>
         </div>
     )
