dc8eea804c7bb3ef8c5c2e21a6d4c290d5f60ce7
manas6699 <manasbaroi6699@gmail.com>
2025-08-13 16:12:24 +0530

    Report page added

---
+++ src/app/admin/Dashboard/Report/page.tsx
@@ -0,0 +1,75 @@
+
+'use client';
+
+
+import Navbar from '@/components/AdminComponents/Navbar'
+import Sidebar from '@/components/AdminComponents/Sidebar'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { SHOW_ALL_ASSIGNS_API } from '@/config/api';
+import ReportTable from '@/components/AdminComponents/ReportTable';
+
+
+type Assign = {
+    _id: string;
+    lead_id: string;
+    assignee_id: string;
+    assignee_name: string;
+    status: string;
+    remarks: string;
+    history: string[];
+    lead_details: {
+        name: string;
+        email: string;
+        phone: string;
+        source: string;
+        status: string;
+        comments: string,
+        location: string,
+        alternate_phone: string,
+        client_budget: string,
+        furnished_status: string,
+        interested_project: string,
+        lead_status: string,
+        preferred_configuration: string,
+        preferred_floor: string,
+        property_status: string,
+        createdAt: string;
+        updatedAt: string;
+    };
+};
+
+const ReportPage = () => {
+    const [assigns, setAssigns] = useState<Assign[]>([]);
+
+    useEffect(() => {
+        const fetchAssigns = async () => {
+            try {
+                const res = await axios.get(SHOW_ALL_ASSIGNS_API);
+                console.log('Assigns API Response:', res.data);
+                if (res.data && res.data.success) {
+                    setAssigns([...res.data.data].reverse());
+                }
+            } catch (err) {
+                console.error('Error fetching assigns:', err);
+            }
+        };
+
+        fetchAssigns();
+    }, []);
+    return (
+        <div>
+            <Navbar />
+            <main className="flex flex-col md:flex-row w-full min-h-screen bg-gray-100">
+                <Sidebar />
+                <div className="flex-1 p-4">
+                    
+                    <ReportTable data={assigns} />
+                </div>
+            
+            </main>
+        </div>
+    )
+}
+
+export default ReportPage
