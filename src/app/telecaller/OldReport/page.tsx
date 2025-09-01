3df8c708ed15ee4218957ba697937a9b27008b95
manas6699 <manasbaroi6699@gmail.com>
2025-09-01 15:52:04 +0530

    Table data ui modified in one liner

--- src/app/telecaller/OldReport/page.tsx
+++ src/app/telecaller/OldReport/page.tsx
@@ -396,9 +396,13 @@ const TelecallerPage = () => {
                                 table.getRowModel().rows.map((row) => (
                                     <tr key={row.id} className="hover:bg-gray-50">
                                         {row.getVisibleCells().map((cell) => (
-                                            <td key={cell.id} className="px-4 py-2 border-b text-sm">
+                                            <td
+                                                key={cell.id}
+                                                className="px-4 py-2 border-b text-sm whitespace-nowrap"
+                                            >
                                                 {flexRender(cell.column.columnDef.cell, cell.getContext())}
                                             </td>
+
                                         ))}
                                     </tr>
                                 ))
