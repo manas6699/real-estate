976750bb9ba0b25d787165a3ba9b8f31c322b15c
manas6699 <manasbaroi6699@gmail.com>
2025-09-05 15:47:50 +0530

    api base url updated

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,7 +2,7 @@
 // export const API_BASE_URL = 'https://split-wise-clone-085p.onrender.com/api';
 // export const API_BASE_URL = 'http://localhost:8000/api';
 
-export const API_BASE_URL = 'http://162.241.115.194:8000/api';
+export const API_BASE_URL = 'https://mmrrealty.co.in/api';
 
 
 export const LEADS_ENDPOINT = `${API_BASE_URL}/mmr/leads`;
