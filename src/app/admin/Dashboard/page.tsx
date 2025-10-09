c7d26ba03886a4b16be314732ddad25e90e6c2de
manas6699 <manasbaroi6699@gmail.com>
2025-10-09 16:07:12 +0530

    fix : Navbar layout changed

--- src/app/admin/Dashboard/page.tsx
+++ src/app/admin/Dashboard/page.tsx
@@ -62,11 +62,11 @@ const Dashboard = () => {
 
     return (
         <div>
-            <Navbar />
             <main className="flex flex-col md:flex-row w-full min-h-screen bg-gray-100">
                 <Sidebar />
-                <div className="flex flex-col md:flex-row flex-1 p-4 gap-4">
+                <div className="flex flex-col md:flex-row flex-1 gap-4 lg:ml-64 p-6">
                     <div className="flex flex-col flex-1 gap-4">
+                        <Navbar />
                         <Overview />
                     </div>
                     {/* <Activity /> */}
