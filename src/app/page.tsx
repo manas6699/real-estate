f12355776783b181ff9f1e9f72d7de453ffdcb3e
manas6699 <manasbaroi6699@gmail.com>
2025-10-21 12:31:54 +0530

    redirect : redireted to another mmr website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,32 @@
-import Footer from "@/components/Footer";
-import Hero from "@/components/Home/Hero";
-import Team from "@/components/Home/Team";
-import Explore from "@/components/Home/Explore";
-import Navbar from "@/components/Home/Navbar";
-import Developer from "@/components/Home/Developer";
-import ChooseUs from "@/components/Home/ChooseUs";
-import StatsSection from "@/components/Home/StatsSection";
+import { redirect } from "next/navigation";
+// import Footer from "@/components/Footer";
+// import Hero from "@/components/Home/Hero";
+// import Team from "@/components/Home/Team";
+// import Explore from "@/components/Home/Explore";
+
+// import Navbar from "@/components/Home/Navbar";
+// import Developer from "@/components/Home/Developer";
+// import ChooseUs from "@/components/Home/ChooseUs";
+// import StatsSection from "@/components/Home/StatsSection";
 // import RecentProperties from "@/components/Home/RecentProperties";
 
 const Home = () => {
 
+  redirect("https://mmrrealty.in/");
 
-  return (
-    <>
-      <Navbar source="home-mmr" />
-      <Hero />
-      <Explore />
-      <StatsSection />
-      {/* <RecentProperties /> */}
-      <Developer />
-      <ChooseUs />
-      <Team />
-      <Footer phoneNumber="7439514475" />
-    </>
-  );
+  // return (
+  //   <>
+  //     <Navbar source="home-mmr" />
+  //     <Hero />
+  //     <Explore />
+  //     <StatsSection />
+  //     <RecentProperties />
+  //     <Developer />
+  //     <ChooseUs />
+  //     <Team />
+  //     <Footer phoneNumber="7439514475" />
+  //   </>
+  // );
 };
 
 export default Home;
